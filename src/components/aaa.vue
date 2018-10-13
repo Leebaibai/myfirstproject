b867b1cd42ff550aa1d9b59f342ddd177f83d868
Leebaibai <410080003@qq.com>
2018-10-14 00:21:14 +0800

    修改

---
+++ src/components/aaa.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>{{msg}}</h1>
+        <h3>{{sss}}</h3>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'test',
+    data(){
+        return {
+            msg:'test',
+            sss:'ttt'
+        }
+    }
+}
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-
+// import aaa from '@/component/aaa'
+import aaa from '../components/aaa'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path:'/name',
+      name:'aaa',
+      component:aaa
     }
   ]
 })
